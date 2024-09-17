2c9042c10af87458409fa57db8f808d8fc6fc527
Leoric01 <troglodit.nick@seznam.cz>
2024-09-17 20:56:06 +0200

    went back to localhost mysql to fix it, everything runs

--- BookNetwork/src/main/java/com/leoric/booknetwork/BookNetworkApplication.java
+++ BookNetwork/src/main/java/com/leoric/booknetwork/BookNetworkApplication.java
@@ -1,7 +1,11 @@
 package com.leoric.booknetwork;
 
+import com.leoric.booknetwork.repositories.RoleRepository;
+import com.leoric.booknetwork.role.Role;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
 
@@ -13,5 +17,14 @@ public class BookNetworkApplication {
     public static void main(String[] args) {
         SpringApplication.run(BookNetworkApplication.class, args);
     }
+
+    @Bean
+	public CommandLineRunner runner(RoleRepository roleRepository) {
+		return args -> {
+			if (roleRepository.findByName("USER").isEmpty()) {
+				roleRepository.save(Role.builder().name("USER").build());
+			}
+		};
+	}
 
 }

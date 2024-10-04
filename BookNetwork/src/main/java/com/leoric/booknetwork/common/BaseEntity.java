c8003fc97b638e30b8adbe8bf7e52b920fba60a1
Leoric01 <troglodit.nick@seznam.cz>
2024-10-04 23:16:01 +0200

    created new entities , everything works so far, app can start

---
+++ BookNetwork/src/main/java/com/leoric/booknetwork/common/BaseEntity.java
@@ -0,0 +1,39 @@
+package com.leoric.booknetwork.common;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @CreatedDate
+    @Column(nullable = false ,updatable = false)
+    private LocalDate createdDate;
+    @CreatedDate
+    @Column(insertable = false)
+    private LocalDate lastModifiedDate;
+    @CreatedDate
+    @Column(nullable = false ,updatable = false)
+    private Integer createdBy;
+    @LastModifiedBy
+    @Column(insertable = false)
+    private Integer lastModifiedBy;
+}
